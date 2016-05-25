1916ac824a9f0bffe29b17227dab0146484d8999
philippequentin91 <philippequentin91@gmail.com>
2016-05-25 04:21:37 -0400

    exo 1 fini

--- prenom.php
+++ prenom.php
@@ -1,7 +1,36 @@
 <?php
-class personne {
+class Personne {
 	protected $id;
 	protected $nom;
 	protected $prenom;
+
+  function __construct($id,$nom,$prenom) {
+    this->id=$id;
+    this->nom=$nom;
+    this->prenom=$prenom;
+  }
+
+  function setNom($nom) {
+    $this->nom = $nom;
+  }
+
+  function getNom() {
+   return $this->nom;
+  }
+
+  function setPrenom($prenom) {
+   $this->prenom = $prenom;
+  }
+
+  function getPrenom() {
+    return $this->prenom;
+  }
+
+  function __toString(){
+    return "id :".$this->id. "nom :".$this->nom. "prenom : ".$this->prenom;
+  }
 }
+
+$eleve = new Personne(2,'SIMONOVIC','Izy');
+echo $eleve;
 ?>
